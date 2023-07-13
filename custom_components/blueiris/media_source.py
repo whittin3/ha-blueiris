15dab37532d3182653751361864a2760533909b6
Neal Whittington <whittin3@gmail.com>
2023-07-13 11:31:30 -0500

    media source working for images, not playing video

--- custom_components/blueiris/media_source.py
+++ custom_components/blueiris/media_source.py
@@ -1,21 +1,20 @@
 """Expose cameras as media sources."""
 from __future__ import annotations
+import logging
 
 from homeassistant.components.media_player import BrowseError, MediaClass
-from homeassistant.components.media_source.error import Unresolvable
 from homeassistant.components.media_source.models import (
     BrowseMediaSource,
     MediaSource,
     MediaSourceItem,
     PlayMedia,
 )
-from homeassistant.components.stream import FORMAT_CONTENT_TYPE, HLS_PROVIDER
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import HomeAssistantError
-from homeassistant.helpers.entity_component import EntityComponent
 
+from . import get_ha
 from .helpers.const import *
-from .models.base_entity import BlueIrisEntity
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_get_media_source(hass: HomeAssistant) -> BlueIrisMediaSource:
@@ -35,35 +34,16 @@ class BlueIrisMediaSource(MediaSource):
 
     async def async_resolve_media(self, item: MediaSourceItem) -> PlayMedia:
         """Resolve media to a url."""
-        component: EntityComponent[BlueIrisEntity] = self.hass.data[DOMAIN]
-        camera = component.get_entity(item.identifier)
+        entity_id = self.hass.data[DATA_BLUEIRIS][DATA_BLUEIRIS_HA_ENTITIES][0]
+        ha = get_ha(self.hass, entity_id)
+        url = ha.api.get_clip_stream_url(item.identifier, 10345040)
+        _LOGGER.info(f"Resolving media url: {url}")
 
-        if not camera:
-            raise Unresolvable(f"Could not resolve media item: {item.identifier}")
-
-        if (stream_type := camera.frontend_stream_type) is None:
-            return PlayMedia(
-                f"/api/camera_proxy_stream/{camera.entity_id}", camera.content_type
-            )
-
-        if "stream" not in self.hass.config.components:
-            raise Unresolvable("Stream integration not loaded")
-
-        try:
-            url = await _async_stream_endpoint_url(self.hass, camera, HLS_PROVIDER)
-        except HomeAssistantError as err:
-            # Handle known error
-            if stream_type != StreamType.HLS:
-                raise Unresolvable(
-                    "Camera does not support MJPEG or HLS streaming."
-                ) from err
-            raise Unresolvable(str(err)) from err
-
-        return PlayMedia(url, FORMAT_CONTENT_TYPE[HLS_PROVIDER])
+        return PlayMedia(url, "image/jpeg")
 
     async def async_browse_media(
-        self,
-        item: MediaSourceItem,
+            self,
+            item: MediaSourceItem,
     ) -> BrowseMediaSource:
         """Return media."""
         if item.identifier:
@@ -72,34 +52,30 @@ class BlueIrisMediaSource(MediaSource):
         can_stream_hls = "stream" in self.hass.config.components
 
         # Root. List cameras.
-        component: EntityComponent[BlueIrisEntity] = self.hass.data[DOMAIN]
         children = []
-        not_shown = 0
-        for blue_iris_entity in component.entities:
-            api = blue_iris_entity.api
-            alerts = api.list_alerts()
-            if stream_type is None:
-                content_type = blue_iris_entity.content_type
+        for entity_id in self.hass.data[DATA_BLUEIRIS][DATA_BLUEIRIS_HA_ENTITIES]:
+            ha = get_ha(self.hass, entity_id)
+            not_shown = 0
+            alerts = await ha.api.list_alerts()
+            for alert in alerts:
+                if True:
+                    content_type = ""
+                else:
+                    not_shown += 1
+                    continue
 
-            elif can_stream_hls and stream_type == StreamType.HLS:
-                content_type = FORMAT_CONTENT_TYPE[HLS_PROVIDER]
-
-            else:
-                not_shown += 1
-                continue
-
-            children.append(
-                BrowseMediaSource(
-                    domain=DOMAIN,
-                    identifier=blue_iris_entity.entity_id,
-                    media_class=MediaClass.VIDEO,
-                    media_content_type=content_type,
-                    title=blue_iris_entity.name,
-                    thumbnail=f"/api/camera_proxy/{blue_iris_entity.entity_id}",
-                    can_play=True,
-                    can_expand=False,
+                children.append(
+                    BrowseMediaSource(
+                        domain=DOMAIN,
+                        identifier=alert.clip,
+                        media_class=MediaClass.VIDEO,
+                        media_content_type=content_type,
+                        title=f"{alert.camera} {alert.date} {alert.memo}",
+                        thumbnail=f"{ha.api.base_url}/thumbs/{alert.path}",
+                        can_play=True,
+                        can_expand=False,
+                    )
                 )
-            )
 
         return BrowseMediaSource(
             domain=DOMAIN,
